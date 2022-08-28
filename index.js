7ab810cd696c2382e6656ee6161bf94819edcafd
Uday Kumar <dudaykumargoud@gmail.com>
2022-08-28 18:24:47 +0530

    updated packages

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 require("dotenv").config();
 const connextDB = require("./db/connectDB");
 const cookiePraser = require("cookie-parser");
 const app = express();
 const isLoggedin = require("./middleware/isLoggedin");
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
